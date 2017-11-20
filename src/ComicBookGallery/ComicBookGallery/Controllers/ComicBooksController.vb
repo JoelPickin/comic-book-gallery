b744242349c9534166229329832122d37259eafe
Joel Pickin <JoelPickin@TPLLP.com>
2017-11-20 17:19:24 +0000

    Updated detail action methos to use action result types

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.vb
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.vb
@@ -1,9 +1,15 @@
 ﻿Imports System
 Imports System.Web.Mvc
 
-Public Class ComicBooksController : Inherits Controller
-    Public Function Detail() As String
-        Return "Hello from the comic books control"
+Public Class ComicBooksController
+    Inherits Controller
+    Public Function Detail() As ActionResult
+        If DateTime.Today.DayOfWeek = DayOfWeek.Monday Then
+            Return Redirect("/")
+        End If
+        Return New ContentResult With {
+            .Content = "Hello from the comic books control"
+        }
     End Function
 
 End Class
