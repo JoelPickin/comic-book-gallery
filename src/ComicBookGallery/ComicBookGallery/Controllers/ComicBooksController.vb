300283d18dfeed9f537b8c6f60eda2294c734c27
Joel Pickin <JoelPickin@TPLLP.com>
2017-11-20 15:29:10 +0000

    Added detail action method

--- src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.vb
+++ src/ComicBookGallery/ComicBookGallery/Controllers/ComicBooksController.vb
@@ -2,5 +2,8 @@
 Imports System.Web.Mvc
 
 Public Class ComicBooksController : Inherits Controller
+    Public Function Detail() As String
+        Return "Hello from the comic books control"
+    End Function
 
 End Class
